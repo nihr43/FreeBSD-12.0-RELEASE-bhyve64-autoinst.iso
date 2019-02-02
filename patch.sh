229a40c858900e015cc27badab2d526412b0f358
Your Name <you@example.com>
2019-02-01 19:06:45 -0600

    32faaf

--- patch.sh
+++ patch.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
+#
+## jenkins job to build freebsd iso for use in bhyve
 
-pkg install -y cdrtools ca_root_nss rsync
+hash cdrtools rsync || sudo pkg install -y cdrtools ca_root_nss rsync
 
-if [ ! -e FreeBSD-12.0-RELEASE-amd64-disc1.iso ]
-then
+rnd () {
+  dd if=/dev/random bs=16 count=1 status=none | md5
+}
+
+[ -e FreeBSD-12.0-RELEASE-amd64-disc1.iso ] || {
  fetch https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/12.0/FreeBSD-12.0-RELEASE-amd64-disc1.iso.xz
  xz -d ./FreeBSD-12.0-RELEASE-amd64-disc1.iso.xz
-fi
+}
 
-if [ -e ./FreeBSD-12.0-RELEASE-bhyve64-autoinst.iso ]
-then
+[ -e ./FreeBSD-12.0-RELEASE-bhyve64-autoinst.iso ] && {
  rm ./FreeBSD-12.0-RELEASE-bhyve64-autoinst.iso
-fi
+}
 
-PATCHED_ISO_DIR=`mktemp -d`
-ORIG_ISO_DIR=`mktemp -d`
+rnd () {
+  dd if=/dev/random bs=16 count=1 status=none | md5
+}
 
-mount -t cd9660 /dev/`mdconfig -f FreeBSD-12.0-RELEASE-amd64-disc1.iso` $ORIG_ISO_DIR
+PATCHED_ISO_DIR="./`rnd`"
+ORIG_ISO_DIR="./`rnd`"
+
+mkdir $PATCHED_ISO_DIR
+mkdir $ORIG_ISO_DIR
+
+sudo mount -t cd9660 /dev/`mdconfig -f FreeBSD-12.0-RELEASE-amd64-disc1.iso` $ORIG_ISO_DIR
 rsync -aq $ORIG_ISO_DIR/ $PATCHED_ISO_DIR/
 
 # make modifications
@@ -27,6 +38,6 @@ cp ./rc.local $PATCHED_ISO_DIR/etc/rc.local
 VOL_ID=$(isoinfo -d -i FreeBSD-12.0-RELEASE-amd64-disc1.iso | grep "Volume id" | awk '{print $3}')
 mkisofs -J -R -no-emul-boot -V "$VOL_ID" -b boot/cdboot -o FreeBSD-12.0-RELEASE-bhyve64-autoinst.iso $PATCHED_ISO_DIR
 
-umount $ORIG_ISO_DIR
+sudo umount $ORIG_ISO_DIR
 rm -rf $ORIG_ISO_DIR
 rm -rf $PATCHED_ISO_DIR
